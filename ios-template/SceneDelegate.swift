5bfefcaf5aa40b179d8d616893716c35e02e4a62
Viranchee Lotia <viranchee@outlook.com>
2021-03-15 15:58:43 +0530

    Remove Storyboard, use Programmatic Launch

--- ios-template/SceneDelegate.swift
+++ ios-template/SceneDelegate.swift
@@ -12,7 +12,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
   
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    guard let _ = (scene as? UIWindowScene) else { return }
+    //  Open ViewController Programatically
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+    window?.windowScene = windowScene
+    window?.rootViewController = ViewController()
+    window?.makeKeyAndVisible()
+    
   }
   
 }
